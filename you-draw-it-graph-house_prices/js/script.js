e9ef4c0d2ad2b4ef8e0b2b1664dcd1fc774c95c0
Rachel Lavin <rachel.lavin@hotmail.com>
2020-01-07 18:41:53 +0000

    Add files via upload

---
+++ you-draw-it-graph-house_prices/js/script.js
@@ -0,0 +1,252 @@
+var data = [
+
+  {"year": 2005.0, "debt":100},
+  {"year": 2005.08333, "debt":100.8},
+  {"year": 2005.166, "debt":101.4},
+  {"year": 2005.249, "debt":102.1},
+  {"year": 2005.332, "debt":102.8},
+  {"year": 2005.415, "debt":103.7},
+  {"year": 2005.498, "debt":105.3},
+  {"year": 2005.581, "debt":107.1},
+  {"year": 2005.664, "debt":108.2},
+  {"year": 2005.747, "debt":110.6},
+  {"year": 2005.83, "debt":111.5},
+  {"year": 2005.913, "debt":112.6},
+  {"year": 2006.0, "debt":113.2},
+  {"year": 2006.08333, "debt":113.5},
+  {"year": 2006.166, "debt":114.1},
+  {"year": 2006.249, "debt":115.8},
+  {"year": 2006.332, "debt":118.2},
+  {"year": 2006.415, "debt":120.3},
+  {"year": 2006.498, "debt":123},
+  {"year": 2006.581, "debt":125.8},
+  {"year": 2006.664, "debt":126.8},
+  {"year": 2006.747, "debt":127.6},
+  {"year": 2006.83, "debt":127.6},
+  {"year": 2006.913, "debt":128.6},
+  {"year": 2007.0, "debt":129.7},
+  {"year": 2007.08333, "debt":130.3},
+  {"year": 2007.166, "debt":130.5},
+  {"year": 2007.249, "debt":131},
+  {"year": 2007.332, "debt":130.6},
+  {"year": 2007.415, "debt":130.3},
+  {"year": 2007.498, "debt":130.6},
+  {"year": 2007.581, "debt":130.4},
+  {"year": 2007.664, "debt":130.4},
+  {"year": 2007.747, "debt":130},
+  {"year": 2007.83, "debt":130},
+  {"year": 2007.913, "debt":129.2},
+  {"year": 2008.0, "debt":128.1},
+  {"year": 2008.08333, "debt":127},
+  {"year": 2008.166, "debt":125.9},
+  {"year": 2008.249, "debt":124.8},
+  {"year": 2008.332, "debt":123.5},
+  {"year": 2008.415, "debt":122.3},
+  {"year": 2008.498, "debt":121.6},
+  {"year": 2008.581, "debt":120.3},
+  {"year": 2008.664, "debt":118.8},
+  {"year": 2008.747, "debt":116.2},
+  {"year": 2008.83, "debt":114.1},
+  {"year": 2008.913, "debt":111.9},
+  {"year": 2009.0, "debt":109},
+  {"year": 2009.08333, "debt":106},
+  {"year": 2009.166, "debt":103.2},
+  {"year": 2009.249, "debt":100.9},
+  {"year": 2009.332, "debt":98.5},
+  {"year": 2009.415, "debt":96.9},
+  {"year": 2009.498, "debt":95.5},
+  {"year": 2009.581, "debt":94.3},
+  {"year": 2009.664, "debt":94.2},
+  {"year": 2009.747, "debt":93.1},
+  {"year": 2009.83, "debt":92.8},
+  {"year": 2009.913, "debt":91.5},
+  {"year": 2010.0, "debt":90.6},
+  {"year": 2010.08333, "debt":89.2},
+  {"year": 2010.166, "debt":88.1},
+  {"year": 2010.249, "debt":87.6},
+  {"year": 2010.332, "debt":86.1},
+  {"year": 2010.415, "debt":85.1},
+  {"year": 2010.498, "debt":84.2},
+  {"year": 2010.581, "debt":84},
+  {"year": 2010.664, "debt":83},
+  {"year": 2010.747, "debt":81.6},
+  {"year": 2010.83, "debt":79.9},
+  {"year": 2010.913, "debt":78.7},
+  {"year": 2011.0, "debt":78},
+  {"year": 2011.08333, "debt":76.5},
+  {"year": 2011.166, "debt":74.7},
+  {"year": 2011.249, "debt":73.7},
+  {"year": 2011.332, "debt":72.4},
+  {"year": 2011.415, "debt":71.3},
+  {"year": 2011.498, "debt":69.8},
+  {"year": 2011.581, "debt":68},
+  {"year": 2011.664, "debt":66.6},
+  {"year": 2011.747, "debt":65.4},
+  {"year": 2011.83, "debt":64.3},
+  {"year": 2011.913, "debt":63.3},
+  {"year": 2012.0, "debt":62.3},
+  {"year": 2012.08333, "debt":60.9},
+  {"year": 2012.166, "debt":61.2},
+  {"year": 2012.249, "debt":60.2},
+  {"year": 2012.332, "debt":60},
+  {"year": 2012.415, "debt":59.9},
+  {"year": 2012.498, "debt":60.5},
+  {"year": 2012.581, "debt":60.6},
+  {"year": 2012.664, "debt":61.1},
+  {"year": 2012.747, "debt":61.3},
+  {"year": 2012.83, "debt":61.1},
+  {"year": 2012.913, "debt":61.3},
+  {"year": 2013.0, "debt":60.2},
+  {"year": 2013.08333, "debt":59.5},
+  {"year": 2013.166, "debt":58.7},
+  {"year": 2013.249, "debt":59},
+  {"year": 2013.332, "debt":59},
+  {"year": 2013.415, "debt":60.5},
+  {"year": 2013.498, "debt":62},
+  {"year": 2013.581, "debt":62.8},
+  {"year": 2013.664, "debt":63.8},
+  {"year": 2013.747, "debt":64.2},
+  {"year": 2013.83, "debt":64.5},
+  {"year": 2013.913, "debt":65.3},
+  {"year": 2014.0, "debt":65.4},
+  {"year": 2014.08333, "debt":65.6},
+  {"year": 2014.166, "debt":66.1},
+  {"year": 2014.249, "debt":67.5},
+  {"year": 2014.332, "debt":69.3},
+  {"year": 2014.415, "debt":71.3},
+  {"year": 2014.498, "debt":73.5},
+  {"year": 2014.581, "debt":74.9},
+  {"year": 2014.664, "debt":76.5},
+  {"year": 2014.747, "debt":77.3},
+  {"year": 2014.83, "debt":77.6},
+  {"year": 2014.913, "debt":77},
+  {"year": 2015.0, "debt":77.3},
+  {"year": 2015.08333, "debt":77},
+  {"year": 2015.166, "debt":77.3},
+  {"year": 2015.249, "debt":77.9},
+  {"year": 2015.332, "debt":78.8},
+  {"year": 2015.415, "debt":80.1},
+  {"year": 2015.498, "debt":80.7},
+  {"year": 2015.581, "debt":81.7},
+  {"year": 2015.664, "debt":82},
+  {"year": 2015.747, "debt":82.9},
+  {"year": 2015.83, "debt":82.6},
+  {"year": 2015.913, "debt":82.5},
+  {"year": 2016.0, "debt":83.2},
+  {"year": 2016.08333, "debt":82.7},
+  {"year": 2016.166, "debt":83},
+  {"year": 2016.249, "debt":83.5},
+  {"year": 2016.332, "debt":83.7},
+  {"year": 2016.415, "debt":84.5},
+  {"year": 2016.498, "debt":86.4},
+  {"year": 2016.581, "debt":87.6},
+  {"year": 2016.664, "debt":88.6},
+  {"year": 2016.747, "debt":89.1},
+  {"year": 2016.83, "debt":90.2},
+  {"year": 2016.913, "debt":89.9},
+  {"year": 2017.0, "debt":90.5},
+  {"year": 2017.08333, "debt":90.7},
+  {"year": 2017.166, "debt":91.1},
+  {"year": 2017.249, "debt":91.4},
+  {"year": 2017.332, "debt":92.8},
+  {"year": 2017.415, "debt":94.1},
+  {"year": 2017.498, "debt":96.4},
+  {"year": 2017.581, "debt":97.9},
+  {"year": 2017.664, "debt":99.2},
+  {"year": 2017.747, "debt":99.5},
+  {"year": 2017.83, "debt":100.3},
+  {"year": 2017.913, "debt":100.8},
+  {"year": 2018.0, "debt":101.2},
+  {"year": 2018.08333, "debt":102},
+  {"year": 2018.166, "debt":102.6},
+  {"year": 2018.249, "debt":103.6},
+  {"year": 2018.332, "debt":104.3},
+  {"year": 2018.415, "debt":105.3},
+  {"year": 2018.498, "debt":106},
+  {"year": 2018.581, "debt":106.6},
+  {"year": 2018.664, "debt":107.6},
+  {"year": 2018.747, "debt":107.8},
+  {"year": 2018.83, "debt":107.5},
+  {"year": 2018.913, "debt":107.1},
+  {"year": 2019.0, "debt":106.5},
+  {"year": 2019.08333, "debt":106.4},
+  {"year": 2019.166, "debt":106.5},
+  {"year": 2019.249, "debt":106.7},
+  {"year": 2019.332, "debt":107},
+  {"year": 2019.415, "debt":107.4},
+  {"year": 2019.498, "debt":108.3},
+  {"year": 2019.581, "debt":108.6},
+  {"year": 2019.664, "debt":108.7},
+  {"year": 2019.747, "debt":108.8},
+
+]
+
+var ƒ = d3.f
+
+var sel = d3.select('#infographie').html('')
+var c = d3.conventions({
+  parentSel: sel,
+  totalWidth: sel.node().offsetWidth,
+  height: 400,
+  margin: {left: 50, right: 50, top: 30, bottom: 30}
+})
+
+c.svg.append('rect').at({width: c.width, height: c.height, opacity: 0})
+
+c.x.domain([2005, 2020])
+c.y.domain([0, 200])
+
+c.xAxis.ticks(20).tickFormat(ƒ())
+c.yAxis.ticks(10).tickFormat(d => d + '')
+
+var area = d3.area().x(ƒ('year', c.x)).y0(ƒ('debt', c.y)).y1(c.height)
+var line = d3.area().x(ƒ('year', c.x)).y(ƒ('debt', c.y))
+
+var clipRect = c.svg
+  .append('clipPath#clip')
+  .append('rect')
+  .at({width: c.x(2010) - 2, height: c.height})
+
+var correctSel = c.svg.append('g').attr('clip-path', 'url(#clip)')
+
+correctSel.append('path.area').at({d: area(data)})
+correctSel.append('path.line').at({d: line(data)})
+yourDataSel = c.svg.append('path.your-line')
+
+c.drawAxis()
+
+yourData = data
+  .map(function(d){ return {year: d.year, debt: d.debt, defined: 0} })
+  .filter(function(d){
+    if (d.year == 2010) d.defined = true
+    return d.year >= 2010
+  })
+
+var completed = false
+
+var drag = d3.drag()
+  .on('drag', function(){
+    var pos = d3.mouse(this)
+    var year = clamp(2009, 2020, c.x.invert(pos[0]))
+    var debt = clamp(0, c.y.domain()[1], c.y.invert(pos[1]))
+
+    yourData.forEach(function(d){
+      if (Math.abs(d.year - year) < .5){
+        d.debt = debt
+        d.defined = true
+      }
+    })
+
+    yourDataSel.at({d: line.defined(ƒ('defined'))(yourData)})
+
+    if (!completed && d3.mean(yourData, ƒ('defined')) == 1){
+      completed = true
+      clipRect.transition().duration(1500).attr('width', c.x(2020))
+    }
+  })
+
+c.svg.call(drag)
+
+
+
+function clamp(a, b, c){ return Math.max(a, Math.min(b, c)) }
