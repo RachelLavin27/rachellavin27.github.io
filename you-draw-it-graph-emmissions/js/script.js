5c0a2957eb34627a9dc5c0d70230b565efac09de
Rachel Lavin <rachel.lavin@hotmail.com>
2020-01-17 12:14:51 +0000

    Add files via upload

--- you-draw-it-graph-emmissions/js/script.js
+++ you-draw-it-graph-emmissions/js/script.js
@@ -1,18 +1,18 @@
 var data = [
 
-  {"year": 2005,    "debt": 47098.82},
-  {"year": 2008,    "debt": 46918.03},
-  {"year": 2009,    "debt": 44330.87},
-  {"year": 2010,    "debt": 44330.87},
-  {"year": 2011,    "debt": 41230.35},
-  {"year": 2012,    "debt": 40757.66},
-  {"year": 2013,    "debt": 41713.48},
-  {"year": 2014,    "debt": 41130.01},
-  {"year": 2015,    "debt": 42363.41},
-  {"year": 2016,    "debt": 43517.55},
-  {"year": 2017,    "debt": 43830.35},
-  {"year": 2018,    "debt": 44463.01},
-  {"year": 2020,    "debt": 44568.69},
+  {"year": 2005,    "debt": 69495.03},
+  {"year": 2008,    "debt": 67301.83},
+  {"year": 2009,    "debt": 61547.23},
+  {"year": 2010,    "debt": 61104.84},
+  {"year": 2011,    "debt": 56989.19},
+  {"year": 2012,    "debt": 57611.53},
+  {"year": 2013,    "debt": 57410.21},
+  {"year": 2014,    "debt": 57098.54},
+  {"year": 2015,    "debt": 59211.81},
+  {"year": 2016,    "debt": 61270.20},
+  {"year": 2017,    "debt": 60743.73},
+  {"year": 2018,    "debt": 61850.55},
+  {"year": 2020,    "debt": 61532.48},
 ]
 
 var ƒ = d3.f
@@ -28,9 +28,9 @@ var c = d3.conventions({
 c.svg.append('rect').at({width: c.width, height: c.height, opacity: 0})
 
 c.x.domain([2005, 2020])
-c.y.domain([0, 65000])
+c.y.domain([0, 100000])
 
-c.xAxis.ticks(10).tickFormat(ƒ())
+c.xAxis.ticks(5).tickFormat(ƒ())
 c.yAxis.ticks(6).tickFormat(d => d + 'kt')
 
 var area = d3.area().x(ƒ('year', c.x)).y0(ƒ('debt', c.y)).y1(c.height)
